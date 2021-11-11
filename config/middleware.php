7a813231230825b030db2bae373a9b5016501958
marescanog <marescanog@gmail.com>
2021-11-11 09:40:58 +0800

    added exception for the home api

--- config/middleware.php
+++ config/middleware.php
@@ -5,7 +5,7 @@ use App\Interfaces\SecretKeyInterface as Secret;
 return function ($app)
 {
     $app->add(new Tuupola\Middleware\JwtAuthentication([
-        "ignore"=>["/auth/login","/user/register","/create-guest","/user/check-number"],
+        "ignore"=>["/auth/login","/user/register","/create-guest","/user/check-number","/"],
         "secret"=>Secret::JWT_SECRET_KEY,
         "error"=>function ($response,$arguments)
         {
