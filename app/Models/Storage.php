e711a2ea27622af994ef355bbd228920e139f53e
marescanog <marescanog@gmail.com>
2021-11-24 22:51:36 +0800

    production build

--- app/Models/Storage.php
+++ app/Models/Storage.php
@@ -15,8 +15,8 @@ class Storage
 
     public function  __construct()
     {
-        // putenv('GOOGLE_APPLICATION_CREDENTIALS=google-credentials.json'); // PROD
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=C:\\users\asus\Downloads\google-credentials.json'); // DEV
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=google-credentials.json'); // PROD
+        //putenv('GOOGLE_APPLICATION_CREDENTIALS=C:\\users\asus\Downloads\google-credentials.json'); // DEV
 
         $this->projectId = 'steel-fin-282304';
 
