49f3ed872a7b980e3d71fca22ac6d8f6b6a2ae58
marescanog <marescanog@gmail.com>
2021-11-28 19:20:03 +0800

    added save and load route for general schedule preference

--- config/routes/worker-routes.php
+++ config/routes/worker-routes.php
@@ -3,4 +3,7 @@
 $app->group("/registration",function() use ($app){
     $app->get("/personal-info","WorkerController:getRegistration_personalInfo");
     $app->post("/save-personal-info","WorkerController:save_personal_info");
+
+    $app->get("/general-schedule","WorkerController:get_general_schedule");
+    $app->post("/save-general-schedule","WorkerController:save_general_schedule");
 });
