3cb2c983d28c6b3e6a74b37143e0d90dc07f14a8
marescanog <marescanog@gmail.com>
2021-12-01 20:32:51 +0800

    added new routes for the project

--- config/routes/file-routes.php
+++ config/routes/file-routes.php
@@ -1,3 +1,14 @@
 <?php
 
 $app->post("/upload","FileController:upload");
+
+$app->get("/search-proj","FileController:searchProj");
+
+
+$app->get("/populate-address-form","FileController:populateAddressForm");
+
+$app->post("/add-address","FileController:addAddress");
+
+$app->post("/add-project","FileController:addProject");
+
+$app->get("/test-route","FileController:sdfsfsd");
