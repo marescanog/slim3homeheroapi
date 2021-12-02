8d2c057d0f5dfed5ac89447134e3227fb3f3f4bc
marescanog <marescanog@gmail.com>
2021-12-02 09:41:47 +0800

    get resolved and get resolved sinfle no longer exist

--- config/routes/ticket-routes.php
+++ config/routes/ticket-routes.php
@@ -3,9 +3,6 @@
 $app->group("/ticket",function() use ($app){
 
     $app->post("/create","SupportTicketController:createTicket");
-    $app->get("/get-resolved","SupportTicketController:getResolved");
-    $app->get("/get-resolved/{id}","SupportTicketController:getResolvedSingle");
-
 
 
     $app->get("/get-all", "SupportTicketController:getAll");
